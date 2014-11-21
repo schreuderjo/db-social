116f31008fda87ed8bf7073185981fb8783490df
Gale Van Rossem & Jordan Schreuder <pair+galeforcevr.schreuderjo@devbootcamp.com>
2014-11-20 22:59:33 -0600

    added rspec and basic css

--- db-social/app/controllers/sessions_controller.rb
+++ db-social/app/controllers/sessions_controller.rb
@@ -3,19 +3,19 @@ class SessionsController < ApplicationController
     if @user = User.find_by(email: params[:user][:email])
       if @user.authenticate(params[:user][:password])
         session[:current_user_id] = @user.id
-        redirect_to posts_path
+        redirect_to '/'
       else
-        flash[:notice] = "Invalid email or password."
+        flash.now[:notice] = "Invalid email or password."
         render '/sessions/index'
       end
     else
-      flash[:notice] = "Invalid email or password."
+      flash.now[:notice] = "Invalid email or password."
       render '/sessions/index'
     end
   end
 
   def destroy
-    session[:current_user_id] = nil
+    session.clear
     flash[:notice] = "You have successfully logged out."
     redirect_to root_url
   end
