01296216aa281e5454916fed0f745ce53ad23994
David Lamps <davidlamps@gmail.com>
2014-11-20 18:44:59 -0600

    basic profile view page, and ability to edit profile if current_user

--- db-social/app/controllers/users_controller.rb
+++ db-social/app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
-    render 'new'
   end
 
   def create
@@ -15,9 +14,28 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    # redirect_to "/#{@user.to_param}" #attempt at changing route to first_name instead of id
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    params[:user].delete(:password) if params[:user][:password].blank?
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :quirk, :avatar, :tweet_size_advice)
   end
 
 end
--- db-social/app/models/user.rb
+++ db-social/app/models/user.rb
@@ -6,9 +6,15 @@ class User < ActiveRecord::Base
   has_many :resources, through: :favorites
 
   validates :email, uniqueness: true
-  validates :email, :password, :first_name, :last_name, presence: true
+  validates :email, :password, :first_name, :last_name, presence: true, on: :update, allow_blank: true
   validates_format_of :email, :with => /[\w|\.]+@\w+\.\w+/, :on => :create
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, on: :update, allow_blank: true
 
   has_secure_password
+
+  #attempt at changing route to first_name instead of id
+  # def to_param
+  #   self.first_name
+  # end
+
 end
