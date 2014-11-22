23f10440a38354056d643739aca703656eda83e2
Gale Van Rossem <galeforcevr@gmail.com>
2014-11-22 15:32:17 -0600

    added classes and css for user profile

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20141120044006) do
     t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
-    t.string   "avatar"
+    t.string   "avatar",            default: "https://d22r54gnmuhwmk.cloudfront.net/rendr-fe/img/default-user-avatar-dc6f2da9.gif"
     t.string   "quirk"
     t.string   "tweet_size_advice"
     t.datetime "created_at"
