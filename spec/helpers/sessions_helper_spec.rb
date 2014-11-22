29f367b73761788d277e428ce8b1c4ba0d2386c2
David Lamps <davidlamps@gmail.com>
2014-11-22 12:38:41 -0600

    added tests for passwords_controller and added s's to 'password' in various files

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 # Specs in this file have access to a helper object that includes
 # the SessionsHelper. For example:
 #
-# describe SessionssHelper do
+# describe SessionsHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
