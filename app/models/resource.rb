6352ee79cf2c1f85733d4ba7ecdcb838dfa2a005
David Lamps <davidlamps@gmail.com>
2014-11-24 07:45:07 -0600

    admins can delete dbc resources and their favorites

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -5,4 +5,12 @@ class Resource < ActiveRecord::Base
   validates_inclusion_of :global_resource, :in => [true, false]
   validates :name, :url, :description, :presence => {:message => "required field missing"}
   validates :description, length: {in: 1..140}
+
+  def destroy_global_favorites
+    if self.favorites.all.length > 0
+      self.favorites.each do |favorite|
+        favorite.destroy
+      end
+    end
+  end
 end
